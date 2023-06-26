679523715734bbfdeaf352b0342530abf035dc34
MPFabio <128690691+MPFabio@users.noreply.github.com>
2023-06-26 14:51:46 +0200

    Update outputs.tf

--- Master/outputs.tf
+++ Master/outputs.tf
@@ -21,3 +21,8 @@ output "The_kubeadm_Public_ip" {
 output "The_kubeadm_Private_ip" {
    value = module.ModuleP.The_kubeadm_Private_ip
 }
+
+output "tls_private_key" {
+  value     = module.ModuleP.tls_private_key.
+  sensitive = true
+}
