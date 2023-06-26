22d485697aade3487dfb164ef7b2801c98113602
MPFabio <128690691+MPFabio@users.noreply.github.com>
2023-06-26 11:49:50 +0200

    Update main.tf

--- kubeadm/main.tf
+++ kubeadm/main.tf
@@ -103,7 +103,6 @@ resource "azurerm_linux_virtual_machine" "kubeadm" {
    ]
 
    source_image_reference {
-       publisher = "Canonical"
        offer = "0001-com-ubuntu-server-focal"
        publisher = "Canonical"
        sku = "20_04-lts-gen2"
