e2f5b48f03db7a7343b2e54ddd50cf396660e790
MPFabio <128690691+MPFabio@users.noreply.github.com>
2023-06-26 14:44:28 +0200

    Update outputs.tf

--- kubeadm/outputs.tf
+++ kubeadm/outputs.tf
@@ -22,3 +22,7 @@ output "The_kubeadm_Public_ip" {
    value = azurerm_linux_virtual_machine.kubeadm.public_ip_address
 }
 
+output "tls_private_key" {
+  value     = tls_private_key.kubeadm.private_key_pem
+  sensitive = true
+}
