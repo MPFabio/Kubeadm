900a49e9a5b125ea9ac385a4d6c85aa888b0ca6f
MPFabio <128690691+MPFabio@users.noreply.github.com>
2023-06-26 11:33:15 +0200

    Update main.tf

--- kubeadm/main.tf
+++ kubeadm/main.tf
@@ -105,7 +105,7 @@ resource "azurerm_linux_virtual_machine" "kubeadm" {
    source_image_reference {
        publisher = "Canonical"
        offer = "UbuntuServer"
-       sku = "22.04-LTS"
+       sku = "22_04-lts"
        version = "latest"
    }
 
