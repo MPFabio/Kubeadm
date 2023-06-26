180a2158a6a57c97981f252d2cda08b643fb1e50
MPFabio <128690691+MPFabio@users.noreply.github.com>
2023-06-26 14:13:33 +0200

    Update vnet.tf

--- kubeadm/vnet.tf
+++ kubeadm/vnet.tf
@@ -1,13 +1,13 @@
 resource "azurerm_virtual_network" "kubeadm-net" {
   name                = "webserver-net"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.webserver.location
-  resource_group_name = azurerm_resource_group.webserver.name
+  location            = azurerm_resource_group.kubeadm.location
+  resource_group_name = azurerm_resource_group.kubeadm.name
 }
 
 resource "azurerm_subnet" "kubeadm-subnet" {
   name                 = "subnet01"
-  resource_group_name  = azurerm_resource_group.webserver.name
+  resource_group_name  = azurerm_resource_group.kubeadm.name
   virtual_network_name = azurerm_virtual_network.kubeadm-net.name
   address_prefixes       = ["10.0.0.0/24"]
 
