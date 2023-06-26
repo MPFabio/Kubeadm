80a80dcdb7151aa4f47dc23dd8908ab4410acb57
MPFabio <128690691+MPFabio@users.noreply.github.com>
2023-06-26 12:43:17 +0200

    Update vnet.tf

--- kubeadm/vnet.tf
+++ kubeadm/vnet.tf
@@ -1,7 +1,7 @@
 resource "azurerm_virtual_network" "kubeadm-net" {
   name                = "webserver-net"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.kubeadm.location
+  location            = data.azurerm_resource_group.kubeadm
   resource_group_name = data.azurerm_resource_group.kubeadm
 }
 
