d9cc253df8751de8072f2fed7bca721bf9086733
MPFabio <128690691+MPFabio@users.noreply.github.com>
2023-06-26 10:48:27 +0200

    Update vnet.tf

--- azure-webserver/vnet.tf
+++ azure-webserver/vnet.tf
@@ -1,14 +1,14 @@
-resource "azurerm_virtual_network" "webserver-net" {
+resource "azurerm_virtual_network" "kubeadm-net" {
   name                = "webserver-net"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.webserver.location
-  resource_group_name = azurerm_resource_group.webserver.name
+  location            = azurerm_resource_group.kubeadm.location
+  resource_group_name = azurerm_resource_group.kubeadm.name
 }
 
-resource "azurerm_subnet" "webserver-subnet" {
+resource "azurerm_subnet" "kubeadm-subnet" {
   name                 = "subnet01"
-  resource_group_name  = azurerm_resource_group.webserver.name
-  virtual_network_name = azurerm_virtual_network.webserver-net.name
+  resource_group_name  = azurerm_resource_group.kubeadm.name
+  virtual_network_name = azurerm_virtual_network.kubeadm-net.name
   address_prefixes       = ["10.0.0.0/24"]
 
   private_link_service_network_policies_enabled = false
