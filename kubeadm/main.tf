78e6b634bd4d5a6f08f0b0109b0368360d6b2346
MPFabio <128690691+MPFabio@users.noreply.github.com>
2023-06-26 11:53:04 +0200

    Update main.tf

--- kubeadm/main.tf
+++ kubeadm/main.tf
@@ -106,6 +106,7 @@ resource "azurerm_linux_virtual_machine" "kubeadm" {
        offer = "0001-com-ubuntu-server-focal"
        publisher = "Canonical"
        sku = "20_04-lts-gen2"
+       version = "latest"
    }
 
    computer_name = "kubeadm${var.environment}"
